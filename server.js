c023237656aaab2dfb9170e9a0906d127992a80c
dev92341 <jasper92341@hotmail.com>
2018-08-01 06:54:04 -0300

    frontend

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: false,
-  historyApiFallback: true
+  historyApiFallback: true,
+  public: 'fierce-sea-80873.herokuapp.com',
+  compress: true
 }).listen(process.env.PORT || 3001, function (err, result) {
   if (err) {
     console.log(err);
